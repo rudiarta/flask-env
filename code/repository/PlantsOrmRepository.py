612e89f5ae481cff48a8b9c748ef42928d9a8166
rudirahardian <rudi.rahardian@ralali.com>
2019-10-13 13:39:44 +0700

    queue with database orm

--- code/repository/PlantsOrmRepository.py
+++ code/repository/PlantsOrmRepository.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from model.PlantsOrmModel import PlantsOrmModel
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 import os
 
@@ -9,24 +10,10 @@ load_dotenv()
 load_dotenv(verbose=True)
 
 engine = create_engine(os.getenv("MYSQL_URL"), echo = True)
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
-
-class Plants(Base):
-    __tablename__ = 'plants'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100))
-    category = Column(String(100))
-    def __init__(self, name, category):
-        self.name = name
-        self.category = category
-
-from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind = engine)
 session = Session()
 
 def addTanaman(name, category):
-    c1 = Plants(name, category)
-    session.add(c1)
+    data = PlantsOrmModel(name, category)
+    session.add(data)
     session.commit()
